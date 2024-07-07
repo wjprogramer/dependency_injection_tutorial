40e5c82d7700a5c892515ef938ca617234736dba
WJ Programmer <weeihua1003@gmail.com>
2024-07-07 17:28:05 +0800

    1.2.2 晚期繫結 late binding

--- hello_di/main.py
+++ hello_di/main.py
@@ -1,8 +1,21 @@
+import configparser
+import importlib
+
 from src.salutation import *
 from src.writers import *
 
 
-if __name__ == '__main__':
-    writer: IMessageWriter = ConsoleMessageWriter()
+def _late_binding_example():
+    config = configparser.ConfigParser()
+    config.read('application.ini')
+
+    module = importlib.import_module('src.writers')
+    type_class = getattr(module, config['DEFAULT']['writer'])
+    writer: IMessageWriter = type_class()
+
     salutation = Salutation(writer)
     salutation.exclaim()
+
+
+if __name__ == '__main__':
+    _late_binding_example()
